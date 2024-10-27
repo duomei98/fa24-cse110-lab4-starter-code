297468222341fdf0d0846013347c47ffe9aead5e
Anna He <130006361+duomei98@users.noreply.github.com>
2024-10-26 19:35:12 -0700

    Exercise 3 done

--- src/components/Remaining.tsx
+++ src/components/Remaining.tsx
@@ -3,7 +3,7 @@ import { AppContext } from "../context/AppContext";
 
 const Remaining = () => {
   const { expenses } = useContext(AppContext);
-  let budget = 1000;
+  const { budget } = useContext(AppContext);
 
   const totalExpenses = expenses.reduce((total, item) => {
     return (total = total + item.cost);
@@ -16,7 +16,7 @@ const Remaining = () => {
   if (remainingBudget < 0) {
     window.alert("You have exceeded your budget!");
   }
-  
+
   return (
     <div className={`alert ${alertType}`}>
       <span>Remaining: ${budget - totalExpenses}</span>
